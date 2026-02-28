e93badf967f4fb17c9931e0a86a7c1dee2fead32
ghifari226 <ghifari226@gmail.com>
2026-02-28 16:44:05 +0700

    Add non negative constraint in books

--- models/library_book.py
+++ models/library_book.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class LibraryBook(models.Model):
     _name = 'library.book'
@@ -7,3 +8,9 @@ class LibraryBook(models.Model):
     title = fields.Char(string='Title')
     author_id = fields.Many2one('library.author', string='Author')
     price = fields.Float(string='Price')
+    
+    @api.constrains('price')
+    def _non_negative_price(self):
+        for record in self:
+            if record.price < 0:
+                raise ValidationError("Price can not be negative.")
